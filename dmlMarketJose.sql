209f51c30a979c3212345dfadb7472468ef0cdca
Nico2795 <nico2795@gmail.com>
2022-11-11 18:07:35 -0300

    poblar tablas minimarketJose

--- dmlMarketJose.sql
+++ dmlMarketJose.sql
@@ -1,13 +1,19 @@
-#Poblar la base con al menos 5 productos#
-#Poblar la base con al menos 4 tipos de productos#
+#Se insertan los datos en cada una de las tablas dependiendo de sus atributos#
+#Se poblan las tablas de tipos de productos con 4 tipos de productos#
+#Se poblan las tablas de producto con 5 productos distintos#
+#Se generan las ventas con productos#
+
+#--------------------VENDEDOR------------------------------#
 INSERT INTO minimarketJose_schema.vendedor (nombre, rut)
 VALUES('Jose', 12628546-8);
+#--------------------CLIENTE--------------------------------------#
 INSERT INTO minimarketJose_schema.cliente (nombre, rut, correo)
 VALUES('Nico', 191866819, 'nico2795@gmail.com');
 INSERT INTO minimarketJose_schema.cliente (nombre, rut, correo)
 VALUES('Marco', 192584961, 'marco@gmail.com');
 INSERT INTO minimarketJose_schema.proveedor (nombre, direccion, telefono)
 VALUES('Nicole', 'casanicole123', 123456789);
+#--------------------TIPOS DE PRODUCTO----------------------------#
 INSERT INTO minimarketJose_schema.tipoProducto (nombre) 
 VALUES('medicamento');
 INSERT INTO minimarketJose_schema.tipoProducto (nombre) 
@@ -16,6 +22,7 @@ INSERT INTO minimarketJose_schema.tipoProducto (nombre)
 VALUES('articulos de aseo');
 INSERT INTO minimarketJose_schema.tipoProducto (nombre) 
 VALUES('alimentos');
+#---------------------PRODUCTO------------------------------------#
 INSERT INTO minimarketJose_schema.producto (nombre, precio, tipoProducto_id)
 VALUES('paracetamol',500, 1);
 INSERT INTO minimarketJose_schema.producto (nombre, precio, tipoProducto_id)
@@ -26,10 +33,13 @@ INSERT INTO minimarketJose_schema.producto (nombre, precio, tipoProducto_id)
 VALUES('pan de molde', 1800, 4);
 INSERT INTO minimarketJose_schema.producto (nombre, precio, tipoProducto_id)
 VALUES('manqueque', 850, 4);
+#-----------------------VENTA-----------------------------------------#
 INSERT INTO minimarketJose_schema.venta(vendedor_id, cliente_id)
 VALUES(1,1);
 INSERT INTO minimarketJose_schema.venta(vendedor_id, cliente_id)
 VALUES(1,2);
+
+#--------------------VENTA DEL PRODUCTO-------------------------------#
 INSERT INTO minimarketJose_schema.venta_producto(venta_id,producto_id, fecha,total)
 VALUES(1,1,'2021-11-11',500);
 INSERT INTO minimarketJose_schema.venta_producto(venta_id,producto_id, fecha,total)
@@ -40,9 +50,7 @@ INSERT INTO minimarketJose_schema.venta_producto(venta_id,producto_id,fecha,tota
 VALUES(2,4,'2021-12-12', 1800);
 INSERT INTO minimarketJose_schema.venta_producto(venta_id,producto_id,fecha,total)
 VALUES(2,4,'2022-11-11', 1800);
-#AGREGAR FECHA, TOTAL#
-#Necesito encontrar las ganancias y ventas en un año, entonces necesitaria el total de precios de las cosas vendidas + la suma de los productos vendidos#
-#Pedir una tabla que tenga los totales con los productos id, y hacer la cuenta de los productos id y la suma de los totales
+
 SELECT * FROM minimarketJose_schema.venta_producto;
 SELECT * FROM minimarketJose_schema.venta;
 SELECT * FROM minimarketJose_schema.vendedor;
